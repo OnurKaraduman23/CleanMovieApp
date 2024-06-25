1a9de76b1aa7b4f0290c65b8d2074ce014ccc4f1
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-06-25 16:27:51 +0300

    Internet connection handler added

--- app/src/main/java/com/example/cleanmovieapp/features/home_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/cleanmovieapp/features/home_fragment/HomeFragment.kt
@@ -3,9 +3,11 @@ package com.example.cleanmovieapp.features.home_fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
+import com.example.cleanmovieapp.common.Resource
 import com.example.cleanmovieapp.databinding.FragmentHomeBinding
 import com.example.cleanmovieapp.features.home_fragment.adapter.PopularMoviesAdapter
 import com.example.moviecaseapp.common.binding_adapter.BindingFragment
@@ -36,10 +38,45 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>() {
 
     private fun collectPopularMoviesUIState() {
         lifecycleScope.launch {
-            homeViewModel.popularMovieList.collectLatest { pagingData ->
-                popularMoviesAdapter.submitData(pagingData)
+            homeViewModel.popularMovieList.collectLatest { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        showProgressBar()
+                    }
+
+                    is Resource.Success -> {
+                        hideProgressBar()
+                        hideError()
+                        popularMoviesAdapter.submitData(resource.data!!)
+                    }
+
+                    is Resource.Error -> {
+                        hideProgressBar()
+                        showError(resource.message)
+
+                    }
+                }
             }
         }
+    }
+
+    private fun showProgressBar() {
+        binding.progressBarHome.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBarHome.visibility = View.INVISIBLE
+    }
+
+    private fun showError(errorMesage: String?) {
+        binding.errorImageView.visibility = View.VISIBLE
+        binding.textViewError.visibility = View.VISIBLE
+        binding.textViewError.text = errorMesage
+    }
+
+    private fun hideError() {
+        binding.errorImageView.visibility = View.INVISIBLE
+        binding.textViewError.visibility = View.INVISIBLE
     }
 
 }
--- app/src/main/java/com/example/cleanmovieapp/features/home_fragment/HomeViewModel.kt
+++ app/src/main/java/com/example/cleanmovieapp/features/home_fragment/HomeViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.cleanmovieapp.features.home_fragment
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.example.cleanmovieapp.common.Resource
 import com.example.cleanmovieapp.domain.use_case.GetPopularMoviesUseCase
 import com.example.themovieapp.domain.model.ui_model.popular_movies.PopularMovie
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,16 +16,19 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getPopularMoviesUseCase: GetPopularMoviesUseCase
+    private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    application: Application
 
-) : ViewModel() {
+) : AndroidViewModel(application) {
 
-    private val _popularMovieList = MutableStateFlow<PagingData<PopularMovie>>(PagingData.empty())
-    val popularMovieList: StateFlow<PagingData<PopularMovie>> get() = _popularMovieList
+    private val _popularMovieList =
+        MutableStateFlow<Resource<PagingData<PopularMovie>>>(Resource.Loading())
+    val popularMovieList: StateFlow<Resource<PagingData<PopularMovie>>> get() = _popularMovieList
 
     init {
         getPopularMovies()
@@ -28,11 +36,38 @@ class HomeViewModel @Inject constructor(
 
     private fun getPopularMovies() {
         viewModelScope.launch {
-            getPopularMoviesUseCase.getPopularMovies()
-                .cachedIn(viewModelScope)
-                .collectLatest { pagingData ->
-                    _popularMovieList.value = pagingData
+            if (hasInternetConnection()) {
+                _popularMovieList.value = Resource.Loading()
+                val result = runCatching {
+                    getPopularMoviesUseCase.getPopularMovies()
+                        .cachedIn(viewModelScope)
+                        .collectLatest { pagingData ->
+                            _popularMovieList.value = Resource.Success(pagingData)
+                        }
                 }
+                result.onFailure { e ->
+                    _popularMovieList.value = Resource.Error(
+                        e.message ?: "Unknown Error"
+                    ) // İnternet bağlantısı dışındaki hata
+                }
+            } else {
+                _popularMovieList.value = Resource.Error("No internet connection")
+            }
+        }
+    }
+
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
         }
     }
 
