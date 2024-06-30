630251803e36e0b1e7bb52e14a46882205a3cda6
Onur KARADUMAN <onury.karaduman@gmail.com>
2024-06-30 21:28:54 +0300

    StringResource initialized

--- app/src/main/java/com/example/cleanmovieapp/CleanMovieApp.kt
+++ app/src/main/java/com/example/cleanmovieapp/CleanMovieApp.kt
@@ -1,7 +1,13 @@
 package com.example.cleanmovieapp
 
 import android.app.Application
+import com.example.cleanmovieapp.common.StringResource
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CleanMovieApp : Application()
+class CleanMovieApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        StringResource.init(this)
+    }
+}
